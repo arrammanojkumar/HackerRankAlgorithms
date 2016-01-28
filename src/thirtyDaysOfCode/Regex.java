cb160ed9ad8522341a698d363e1a00f7c1366989
Manoj Kumar <manoo2mail@gmail.com>
2016-01-28 23:18:13 +0530

    Solved other problem

---
+++ src/thirtyDaysOfCode/Regex.java
@@ -0,0 +1,42 @@
+package thirtyDaysOfCode;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+public class Regex {
+
+	public static void main(String[] args) {
+
+		System.out.println("h");
+
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int n = Integer.parseInt(br.readLine());
+
+			while (n > 0) {
+
+				boolean flag = true;
+				String regex = br.readLine();
+
+				try {
+					Pattern pattern = Pattern.compile(regex);
+				} catch (Exception e) {
+					flag = false;
+				}
+
+				if (flag) {
+					System.out.println("Valid");
+				} else {
+					System.out.println("Invalid");
+				}
+
+				n--;
+			}
+			br.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
