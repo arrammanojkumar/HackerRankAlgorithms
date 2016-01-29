f3fb57ca1d761eb3c42dd988c82c38ba345183aa
Manoj Kumar <manoo2mail@gmail.com>
2016-01-29 23:16:13 +0530

    Completed this contest

---
+++ src/thirtyDaysOfCode/Testing.java
@@ -0,0 +1,29 @@
+package thirtyDaysOfCode;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		//Total no of test cases
+		System.out.println("5");
+		
+		//yes
+		System.out.println("10 9"); 
+		System.out.println("-50 0 64 -14 -56 -91 -65 -36 -51 28"); 
+		
+		//no
+		System.out.println("4 1"); 
+		System.out.println("0 1 3 -6");
+		
+		//yes
+		System.out.println("9 6"); 
+		System.out.println("58 29 35 18 43 -28 -76 43 0");
+		
+		//no
+		System.out.println("11 5"); 
+		System.out.println("-6 88 17 -96 43 83 99 -25 0 -39 -86");
+		
+		//yes
+		System.out.println("6 4"); 
+		System.out.println("74 9 -14 -13 14 0");
+	}
+}
