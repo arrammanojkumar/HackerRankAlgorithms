0d492c32a92332635a5113f263d730c9599de695
Manoj Kumar <manoo2mail@gmail.com>
2018-08-07 16:51:01 +0530

    sample

--- src/algorithms/implementation/easy/MiniMaxSum.java
+++
@@ -1,7 +0,0 @@
-package algorithms.implementation.easy;
-
-/**
- * Created by marram on 8/7/18.
- */
-public class MiniMaxSum {
-}
--- src/thirtyDaysOfCode/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package thirtyDaysOfCode;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World.\nWelcome to 30 Days of Code.");
-	}
-}
