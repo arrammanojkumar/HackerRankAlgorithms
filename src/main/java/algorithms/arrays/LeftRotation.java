c282299a3e7b290fae90bee7f5cbfc2bd1d8bf68
Manoj Kumar <manoo2mail@gmail.com>
2019-06-03 23:59:56 +0530

    solved arrays left rotation

--- src/main/java/algorithms/arrays/LeftRotation.java
+++ src/main/java/algorithms/arrays/LeftRotation.java
@@ -12,42 +12,29 @@ public class LeftRotation {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        String[] nd = scanner.nextLine().split(" ");
 
-//        String[] nd = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(nd[0]);
+//        int n = 5;
 
-//        int n = Integer.parseInt(nd[0]);
-        int n = 5;
+        int d = Integer.parseInt(nd[1]);
+//        int d = 4;
 
-//        int d = Integer.parseInt(nd[1]);
-        int d = 4;
+        int[] a = new int[n];
+//        int[] a = {1, 2, 3, 4, 5};
 
-//        int[] a = new int[n];
-        int[] a = {1, 2, 3, 4, 5};
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-//        String[] aItems = scanner.nextLine().split(" ");
-//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-//
-//        for (int i = 0; i < n; i++) {
-//            int aItem = Integer.parseInt(aItems[i]);
-//            a[i] = aItem;
-//        }
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
 
         int[] result = rotLeft(a, d);
         for(int item : result){
             System.out.print(item +" ");
         }
-//        for (int i = 0; i < result.length; i++) {
-//            bufferedWriter.write(String.valueOf(result[i]));
-//
-//            if (i != result.length - 1) {
-//                bufferedWriter.write(" ");
-//            }
-//        }
-//
-//        bufferedWriter.newLine();
-//
-//        bufferedWriter.close();
 
         scanner.close();
     }
