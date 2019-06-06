d0d1ea140f095977487e95707b81af3332b2ca48
Manoj Kumar <manoo2mail@gmail.com>
2019-06-06 23:41:42 +0530

    completed problem

--- src/main/java/algorithms/arrays/New_Year_Chaos.java
+++ src/main/java/algorithms/arrays/New_Year_Chaos.java
@@ -1,33 +1,28 @@
 package algorithms.arrays;
 
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class New_Year_Chaos {
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-//        int t = scanner.nextInt();
-        int t = 2;
-//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         for (int tItr = 0; tItr < t; tItr++) {
-//            int n = scanner.nextInt();
-            int n = 5;
-//            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-//            int[] q = new int[n];
-            int[] q = {2, 1, 5, 3, 4};
+            int[] q = new int[n];
 
-//            String[] qItems = scanner.nextLine().split(" ");
-//            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-//
-//            for (int i = 0; i < n; i++) {
-//                int qItem = Integer.parseInt(qItems[i]);
-//                q[i] = qItem;
-//            }
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
 
             minimumBribes(q);
             break;
