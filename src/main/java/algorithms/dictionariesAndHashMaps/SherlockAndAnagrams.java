39c1a52ca3b9f719031f182d884a255236f7fd2d
Manoj Kumar <manoo2mail@gmail.com>
2019-06-10 17:06:04 +0530

    Solved Sherlock problem

--- src/main/java/algorithms/dictionariesAndHashMaps/SherlockAndAnagrams.java
+++ src/main/java/algorithms/dictionariesAndHashMaps/SherlockAndAnagrams.java
@@ -1,7 +1,5 @@
 package algorithms.dictionariesAndHashMaps;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -17,22 +15,14 @@ public class SherlockAndAnagrams {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
         int q = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         for (int qItr = 0; qItr < q; qItr++) {
             String s = scanner.nextLine();
 
             int result = sherlockAndAnagrams(s);
-
-            bufferedWriter.write(String.valueOf(result));
-            bufferedWriter.newLine();
+            System.out.println(result);
         }
-
-        bufferedWriter.close();
-
         scanner.close();
     }
 
